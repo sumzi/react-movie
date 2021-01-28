233af86fa199a32921a14d9a1dd56df32a2bcc39
sumzi <wltn1873@gmail.com>
2021-01-29 02:39:13 +0900

    update Landing Page

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,7 +1,4 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
-
-
-export const API_URL='https://openapi.naver.com/v1/search/movie.json';
-
-export const IMAGE_BASE_URL='https://openapi.naver.com/v1/search/image'
+export const API_URL='https://api.themoviedb.org/3/';
+export const IMAGE_BASE_URL='http://image.tmdb.org/t/p/'
