ef643685f973b6c98ef88796c9b501af2cf51957
sumzi <wltn1873@gmail.com>
2021-01-28 22:54:04 +0900

    first commit

---
+++ client/src/components/Config.js
@@ -0,0 +1,7 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+
+export const API_URL='https://openapi.naver.com/v1/search/movie.json';
+
+export const IMAGE_BASE_URL='https://openapi.naver.com/v1/search/image'
