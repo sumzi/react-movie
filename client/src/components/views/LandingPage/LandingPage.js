3c98e576a09b623c73a36f22b697e472b7837213
sumzi <wltn1873@gmail.com>
2021-01-31 02:00:00 +0900

    Favorite button Added

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,57 +1,79 @@
-import React,{ useEffect,useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
-import { API_URL, IMAGE_BASE_URL } from '../../Config';
-import {API_KEY} from '../../key';
-import MainImage from './Sections/MainImage';
+import { API_URL, IMAGE_BASE_URL } from "../../Config";
+import { API_KEY } from "../../key";
+import MainImage from "./Sections/MainImage";
+import GridCards from "../commons/GridCards";
+import { Row,Button } from "antd";
 
 function LandingPage() {
+  const [Movies, setMovies] = useState([]);
+  const [MainMovieImage, setMainMovieImage] = useState(null);
+  const [CurrentPage, setCurrentPage] = useState(0);
 
-    
-    const [Movies, setMovies] = useState([])
-    const [MainMovieImage,setMainMovieImage]=useState(null)
-    
-    useEffect(() => {
-        const endpoint=`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+  useEffect(() => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    fetchMovies(endpoint);
+  }, []);
 
+  const fetchMovies = (endpoint) => {
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setMovies([...Movies, ...response.results]);
+        setMainMovieImage(response.results[0]);
+        setCurrentPage(response.page);
+      });
+  };
 
-        fetch(endpoint)
-        .then(response=>response.json())
-        .then(response=>{
-            console.log(response)
-            
-            setMovies([...Movies,response.results])
-            setMainMovieImage(response.results[0])
-        
-        
-        })
-    }, [])
+  const loadMoreItems = () => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${
+      CurrentPage + 1
+    }`;
 
+    fetchMovies(endpoint);
+  };
+  return (
+    <div style={{ width: "100%", margin: "0" }}>
+      {/* Main Image */}
 
+      {MainMovieImage && (
+        <MainImage
+          image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+          title={MainMovieImage.original_title}
+          text={MainMovieImage.overview}
+        />
+      )}
 
-    return (
-        <div style={{width:'100%',margin:'0'}}>
-            {/* Main Image */}
-
-            {MainMovieImage &&
-                <MainImage
-                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
-                    title={MainMovieImage.original_title}
-                    text={MainMovieImage.overview}
+      <div style={{ width: "85%", margin: "1rem auto" }}>
+        <h2>movies by latest</h2>
+        <hr />
+        {/* Movie Grid Cards */}
+        <Row gutter={[16, 16]}>
+          {Movies &&
+            Movies.map((movie, index) => (
+              <React.Fragment key={index}>
+                <GridCards
+                  landingPage
+                  image={
+                    movie.poster_path
+                      ? `${IMAGE_BASE_URL}w500${movie.poster_path}`
+                      : null
+                  }
+                  movieId={movie.id}
+                  movieName={movie.original_title}
                 />
-            }
+              </React.Fragment>
+            ))}
+        </Row>
+      </div>
 
-            <div style={{width:'85%',margin:'1rem auto'}}>
-                <h2>movies by latest</h2>
-                <hr/>
-                {/* Movie Grid Cards */}
-
-            </div>
-
-            <div style={{display:'flex',justifyContent:'center'}}>
-                <button>Load More</button>
-            </div>
-        </div>
-    )
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Button onClick={loadMoreItems}>Load More</Button>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
